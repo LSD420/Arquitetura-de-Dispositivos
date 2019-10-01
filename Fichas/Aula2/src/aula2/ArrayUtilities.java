7227c328c7f9cb41627c434c047b615234103f35
LSD420 <lenioxd@hotmail.com>
2019-10-01 21:24:40 +0100

    ficha2

--- Fichas/Aula2/src/aula2/ArrayUtilities.java
+++ Fichas/Aula2/src/aula2/ArrayUtilities.java
@@ -10,5 +10,101 @@ package aula2;
  * @author Turma A
  */
 public class ArrayUtilities {
+    public static String toString(int[] a){
+        String str = "{";
+        for (int i = 0; i < a.length; i++) {
+            if (i== a.length){str += a[i]+"]";}
+            else{str += a[i]+",";}
+            
+            str += a[i];
+        }
+        
+        return str+"}";
+            
+    }
+    public static int maximumOf(int[] a) {
+        int max = a[0];        
+        for (int i = 1; i < a.length; i++) {
+            if(a[i] > max)
+                max = a[i];
+        }
+        return max;
+    }
+    public static int minimumOf(int[] a) {
+        int min = a[0];        
+        for (int i = 1; i < a.length; i++) {
+            if(a[i] < min)
+                min = a[i];
+        }
+        return min;
+    }
+    public static int[] copyOf(int[] a){
+        int[] copia = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            copia[i] = a[i];
+        }
+        
+        return copia;
+    }
+    public static boolean contains(int[] a, int b){
+        boolean vf = false;
+        for (int i = 0; i < a.length; i++) {
+            if (b == a[i]) {
+                vf = true;
+            }
+        }
+        return vf;
+    }
+    public static boolean containsDuplicates(int[] a){
+        boolean vf = false;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 1; j < a.length -1; j++) {
+                if(a[i]==a[j]) vf=true;
+            }
+        }
+        return vf;
+    }
+    public static int indexOf(int[] a, int b) {
+        int index = -1;
+        for (int i = 0; i < a.length; i++) {
+            if (b == a[i]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static int[] add(int[] a, int b){
+        int[] adicionar = new int[a.length+1];
+        for (int i = 0; i < a.length; i++) {
+            adicionar[i] = a[i];
+        }
+        adicionar[adicionar.length-1] = b;
+        return adicionar;
+    } 
+    public static int[] remove(int[] a, int b){
+        int cont =0 ;
+        int pos=0;
+        for (int j = 0; j < a.length; j++) {
+            if (a[j] == b) {
+                cont +=1;
+            }
+        }
+        int[] remover = new int[a.length-cont];
+        for (int i = 0; i < a.length; i++) {
+            if (b!= a[i] ) {
+                 remover[pos++] = a[i];
+            }
+        }
+        /*for (int i = 0; i < a.length; i++) {
+            remover[i] = a[i];
+        }
+        for (int j = 0; j < remover.length; j++) {
+            if (remover[j] == b ) {
+                
+            }
+        }
+        //remover[numero] = b; */
+        return remover; 
+    }
     
 }
--- Fichas/Aula2/src/aula2/Aula2.java
+++ Fichas/Aula2/src/aula2/Aula2.java
@@ -5,6 +5,8 @@
  */
 package aula2;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Turma A
@@ -24,7 +26,29 @@ public class Aula2 {
         System.out.println(e);
         boolean f = NumericalUtilities.isPrime(2);
         System.out.println(f);
+        System.out.println("Parte2");
+        int[] arraya = {1,2,3};
+        String aa = ArrayUtilities.toString(arraya);
+        System.out.println(aa);
+        int[] arrayb = {2,54,23,1,6,34,9};
+        int bb = ArrayUtilities.maximumOf(arrayb);
+        System.out.println(bb);
+        int cc = ArrayUtilities.minimumOf(arrayb);
+        System.out.println(cc);
+        int[] arrayd = {4,2,3,41,23,213,4};
+        int[] dd = ArrayUtilities.copyOf(arrayd);
+        System.out.println(Arrays.toString(dd));
+        boolean ee = ArrayUtilities.contains(arrayd, 4);
+        System.out.println(ee);
+        boolean ff = ArrayUtilities.containsDuplicates(arrayd);
+        System.out.println(ff);
+        int gg = ArrayUtilities.indexOf(arrayb, 1);
+        System.out.println(gg);
+        int[] hh = ArrayUtilities.add(arrayd,4);
+        System.out.println(Arrays.toString(hh));
+        int[] ii = ArrayUtilities.remove(arrayd,213);
+        System.out.println(Arrays.toString(ii));
         
-    
+        
     }    
 }
